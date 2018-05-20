1f59aca1e5afa2da4eda4fb4b15df2b9ac7647aa
jettFeng <1130034920@>
2018-05-20 12:42:47 +0800

    webpack.config.js

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,4 +1,5 @@
-let path = require('path')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     //entry:['./src/script/main.js','./src/script/a.js'],//数组形式
     entry:{
@@ -6,7 +7,14 @@ module.exports = {
         a:'./src/script/a.js'
     },//对象形式
     output:{
-        path:path.resolve(__dirname,'dist/js'),//出口必修是绝对路径
-        filename:'[name]-bundle-[id]-[hash].js'//[name],[id],[hash]随意组合配置
-    }
+        path:path.resolve(__dirname,'dist'),//出口必修是绝对路径
+        filename:'js/[name].js'//[name],[id],[hash]随意组合配置
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template:'src/index.html',
+            filename:'index2.html',
+            inject:'head'
+        })
+    ]
 }
