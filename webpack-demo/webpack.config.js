4d2824e8c67d05ae20b935361300334907308f76
jettFeng <1130034920@>
2018-05-20 12:09:28 +0800

    lesson2-2

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,8 +1,12 @@
 let path = require('path')
 module.exports = {
-    entry:'./src/script/main.js',
+    //entry:['./src/script/main.js','./src/script/a.js'],//数组形式
+    entry:{
+        main:'./src/script/main.js',
+        a:'./src/script/a.js'
+    },//对象形式
     output:{
         path:path.resolve(__dirname,'dist/js'),//出口必修是绝对路径
-        filename:'bundle.js'
+        filename:'[name]-bundle-[id]-[hash].js'//[name],[id],[hash]随意组合配置
     }
 }
